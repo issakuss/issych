9eb0ee831b2ae8b514788f2322da7cb77c371df6
issakuss <issakuss@gmail.com>
2023-07-06 09:48:18 +0900

    feat: Implement some stat function
    - arcsine_sqrt()
    - iqr()
    - nanzscore()

--- issych/stat.py
+++ issych/stat.py
@@ -1,4 +1,7 @@
-from typing import Optional
+from typing import Optional, Sequence
+
+import numpy as np
+import pandas as pd
 
 
 class Pvalue2SigMark:
@@ -19,3 +22,29 @@ class Pvalue2SigMark:
             if pvalue < thr:
                 return mark
         return self.ns_comment
+
+
+def arcsine_sqrt(r: float) -> float:
+    return np.arcsin(np.sqrt(r))
+
+
+def iqr(vec_origin: Sequence[float] | pd.Series) -> np.ndarray:
+    vec_dropna = pd.Series(vec_origin).dropna()
+    vec_to_ret = np.array(vec_origin)
+    desc = vec_dropna.describe()
+    low = desc['25%']
+    high = desc['75%']
+    iqr = high - low
+
+    vec_to_ret[(low < vec_origin) & (vec_origin < high)] = 0.
+
+    are_higher = (vec_origin - high) > 0.
+    are_lower = (vec_origin - low) < 0.
+    vec_to_ret[are_higher] = (vec_origin[are_higher] - high) / iqr
+    vec_to_ret[are_lower] = (low - vec_origin[are_lower]) / iqr * -1
+    return vec_to_ret
+
+
+def nanzscore(vec: Sequence[float] | pd.Series) -> np.ndarray:
+    vec = np.array(vec)
+    return (vec - np.nanmean(vec)) / np.nanstd(vec)

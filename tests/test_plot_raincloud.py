f85ed1d9e64cc9a294188811d8118ccf31bc5707
issakuss <issakuss@gmail.com>
2024-01-19 16:37:15 +0900

    feat: Enhance raincloud plot positioning

--- tests/test_plot_raincloud.py
+++ tests/test_plot_raincloud.py
@@ -2,12 +2,14 @@ import unittest
 
 import seaborn as sns
 
-from issych.figure import prepare_ax, plot_raincloud
+from issych.figure import prepare_axes, plot_raincloud
 
 
 class TestPlotRaincloud(unittest.TestCase):
     def test(self):
         data = sns.load_dataset('tips')
-        fig, ax = prepare_ax(path_ini='tests/testdata/config/rcparams.ini')
-        plot_raincloud(data, ax, x='day', y='total_bill', box=True)
+        fig, axes = prepare_axes(
+            2, 1, path_ini='tests/testdata/config/rcparams.ini')
+        plot_raincloud(data, axes[0], x='day', y='total_bill', box=True)
+        plot_raincloud(data.total_bill, axes[1], box=True)
         fig.savefig('test_plot_raincloud.png')

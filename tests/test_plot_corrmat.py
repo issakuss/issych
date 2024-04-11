17629c352cdd565b17492e5fa3257a1948a1074a
issakuss <issakuss@gmail.com>
2024-04-11 10:32:22 +0900

    feat: Enhance functions
    - plot_dist() no longer requires lw and abbr arg
    - Axes typealias supports _AxesBase

--- tests/test_plot_corrmat.py
+++ tests/test_plot_corrmat.py
@@ -11,6 +11,9 @@ class TestPlotCorrmat(unittest.TestCase):
         data = sns.load_dataset('iris')
         data = pd.get_dummies(data, prefix='iris', dtype=int)
         abbr = load_config('tests/testdata/config/abbr.ini').flatten()
+        g = plot_corrmat(data, path_ini='tests/testdata/config/rcparams.ini')
+        g = plot_corrmat(data, path_ini='tests/testdata/config/rcparams.ini',
+                         abbr=abbr)
         g = plot_corrmat(data, path_ini='tests/testdata/config/rcparams.ini',
                          abbr=abbr, kwargs_diag={'lw': 0.5})
         g.savefig('test_plot_corrmat.png')

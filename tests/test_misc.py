7bbdf1650551668592e89f01bbb9f7c2a3f35e91
issakuss <issakuss@gmail.com>
2025-04-23 15:53:21 +0900

    🔬 feat: Add group_by_feature_balance()

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,10 +1,16 @@
 import unittest
 from time import sleep
 
+import numpy as np
+import pandas as pd
+import pingouin as pg
+import matplotlib.pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 from joblib import Parallel, delayed
 
-from issych.misc import alphabet2num, meas_exectime, tqdm_joblib
+from issych.misc import (
+    alphabet2num, meas_exectime, tqdm_joblib, group_by_feature_balance)
 
 
 class TestAlphabet2Num(unittest.TestCase):
@@ -36,3 +42,43 @@ class TqdmJobLib(unittest.TestCase):
         with tqdm_joblib(tqdm(total=len(items))) as pbar:
             results = Parallel(n_jobs=-1)(paralleled)
         self.assertEqual(sorted(results), [item * 2 for item in items])
+
+
+class TestGroupByFeatureBalance(unittest.TestCase):
+    def setUp(self):
+        def generate_toydata(rate_male: float, rate_med_exp: float,
+                             mean_age: float, std_age: float, n_samples: int
+                             ) -> pd.DataFrame:
+            is_male = np.random.choice([True, False], size=n_samples,
+                                       p=[rate_male, 1 - rate_male])
+            has_med_exp = np.random.choice([True, False], size=n_samples,
+                                           p=[rate_med_exp, 1 - rate_med_exp])
+            age = np.random.normal(loc=mean_age, scale=std_age, size=n_samples)
+
+            return pd.DataFrame({'is_male': is_male,
+                                 'has_med_exp': has_med_exp,
+                                 'age': age})
+
+        self.toydata = pd.concat([
+            generate_toydata(rate_male=.5, rate_med_exp=.8, mean_age=45,
+                             std_age=12, n_samples=15),
+            generate_toydata(rate_male=.7, rate_med_exp=.1, mean_age=22,
+                             std_age=2, n_samples=35)]).reset_index(drop=True)
+
+    def test(self):
+        data = group_by_feature_balance(
+            self.toydata.reset_index(),
+            id_col='index', cat_col=['is_male', 'has_med_exp'], num_col='age',
+            group_col='group', seed=0)
+        cols = ['is_male', 'has_med_exp', 'age']
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+        for ax, var in zip(axes, cols):
+            sns.violinplot(
+                data=data, y=var, hue='group', split=True, ax=ax)
+        fig.savefig('tests/output/test_group_by_feature_balance.png')
+        data.astype(float).groupby('group').describe()
+
+        MIN_PVAL = 0.05
+        for var in cols:
+            res = pg.ttest(data.query('group')[var], data.query('~group')[var])
+            self.assertGreater(res['p-val'].iloc[0], MIN_PVAL)

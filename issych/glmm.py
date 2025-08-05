8cbc385a205a51d042c664a4e8a32aa008e6992c
issakuss <issakuss@gmail.com>
2025-08-05 15:47:04 +0900

    🔬 feat: Return R version

--- issych/glmm.py
+++ issych/glmm.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Tuple
 import sys
 import os
 import contextlib
@@ -8,6 +8,7 @@ from rpy2.robjects import r, pandas2ri, default_converter
 from rpy2.robjects.conversion import localconverter
 from rpy2.robjects.packages import importr
 from issych.typealias import Number
+from issych.dataclass import Dictm
 
 from .stat import kwargs4r
 
@@ -27,6 +28,12 @@ def _suppress_r_console_output(verbose: bool):
         finally:
             sys.stdout = old_stdout
             sys.stderr = old_stderr
+
+def rver() -> Dictm:
+    return Dictm(
+        major=r('R.version$major')[0],
+        minor=r('R.version$minor')[0],
+        full=r('version$version.string')[0])
 
 class GlmmTMB:
     def __init__(self, data: pd.DataFrame, formula: str,

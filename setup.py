661ab794a51bb0f3a1e13774f0548cd368d20b40
issakuss <issakuss@gmail.com>
2025-08-05 11:54:37 +0900

    🔬 chore: Version up

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='issych',
-    version='0.0.8.1',
+    version='0.0.9.0',
     author='Issaku Kawashima',
     author_email='issakuss@gmail.com',
     description='Convenient tools for psychological studies',
